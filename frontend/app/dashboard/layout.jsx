beddbb88734238a4796c71cde5a6f198778ab51e
Barath G <barathg@b.xd>
2025-07-11 12:00:54 +0530

    done

--- frontend/app/dashboard/layout.jsx
+++ frontend/app/dashboard/layout.jsx
@@ -1,19 +1,65 @@
+"use client"
+import { createContext, useEffect, useState } from "react"
+import { jwtDecode } from "jwt-decode"
 import AppSidebar from './compontents/Appsidebar/AppSidebar'
 import Header from './compontents/Appheaderbar/header'
-import { SidebarProvider, SidebarTrigger , SidebarInset} from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger, SidebarInset } from "@/components/ui/sidebar"
 
-export default function RootLayout({ children }) {
-    return (
-        <div >
-            <SidebarProvider>   
-                <AppSidebar />
-                <SidebarInset>
-                <Header/>
-                <main>
-                    {children}
-                </main>
-                </SidebarInset>
-            </SidebarProvider> 
-        </div>
-    )
+export const fetchdata = createContext();// create thhe fetchdata
+export default  function RootLayout({ children }) {
+  
+ const [showstudentform, setshowstudentform] = useState(false);
+  const [user, setUser] = useState("");
+  const [dashboardResponse, setDashboardResponse] = useState(null); 
+  useEffect(() => {
+    const fetchdata = async () => {
+      const token = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('token='))
+        ?.split('=')[1];
+
+      if (token) {
+        try {
+          const payload = jwtDecode(token);
+          if (payload) setUser(payload);
+        } catch (e) {
+          console.error("Invalid token", e);
+        }
+      }
+    console.log("fecteching clasess...")
+      const res = await fetch("http://localhost:8000/dashboard", {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        }
+      });
+      if (res.status === 440) {
+        setshowstudentform(true);
+      }
+     
+      try {
+        const data = await res.json();
+        setDashboardResponse(data);
+        console.log("Response -->", data);
+      } catch (e) {
+        setDashboardResponse({ error: "Failed to parse response" });
+      }
+    };
+    fetchdata();
+  }, []);
+  return (
+    <fetchdata.Provider value={{dashboardResponse,user,showstudentform,setshowstudentform}}>
+      <div>
+      <SidebarProvider>
+        <AppSidebar  />
+        <SidebarInset>
+          <Header />
+          <main>{children}</main>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+    </fetchdata.Provider>
+    
+  )
 }
